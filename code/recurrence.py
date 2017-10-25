e0e0b81dc8dd2d14b2c61e3f6e2b4ec41684dcd5
jinx <gtxmiracle@buaa.edu.cn>
2017-10-25 18:03:46 +0800

    1

--- code/recurrence.py
+++ code/recurrence.py
@@ -6,9 +6,24 @@
 from code.net_config import config
 import torch
 import torch.nn as nn
-from torch.nn import Conv2d, Linear
+from torch.nn import Conv3d, Linear
 from torch.nn.functional import leaky_relu, max_pool2d
 from torch.autograd import Variable
+
+
+class FCConv_block(nn.Module):
+    def __init__(self):
+        super(FCConv_block, self).__init__()
+        # inchannels????
+        self.conv = Conv3d(256, 128, (3, 3, 3), padding=1)
+        self.fc = Linear(1024, 128 * 4 * 4 * 4)
+
+    def forward(self, x, hidden):
+        out1 = self.fc(x)
+        out1.view(128, 4, 4, 4)
+        out2 = self.conv(hidden)
+
+        return out1 + out2
 
 
 class Recurrence(nn.Module):
